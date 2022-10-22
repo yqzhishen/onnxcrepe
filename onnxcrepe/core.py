Fix wrong axis in weighted argmax decoder & incorrect shape in preprocess

onnxcrepe/core.py
@@ -350,7 +350,7 @@ def preprocess(audio,
         n_bytes = audio.strides[-1]
         frames = np.lib.stride_tricks.as_strided(
             audio[start:end],
-            shape=((end - start - WINDOW_SIZE) // int(hop_length), WINDOW_SIZE),
+            shape=((end - start - WINDOW_SIZE) // int(hop_length) + 1, WINDOW_SIZE),
             strides=(int(hop_length) * n_bytes, n_bytes))  # shape=(batch, 1024)
 
         # Note:

onnxcrepe/decode.py
@@ -24,7 +24,7 @@ def weighted_argmax(logits: np.ndarray):
 
     # Find bounds of analysis window
     start = np.maximum(0, bins - 4)
-    end = np.minimum(logits.shape[2], bins + 5)
+    end = np.minimum(logits.shape[1], bins + 5)
 
     # Mask out everything outside of window
     for batch in range(logits.shape[0]):